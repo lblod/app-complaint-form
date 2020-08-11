544223545638ee63b1925d03e0c4ab293d0632d9
Claire Lovisa <lovisa.claire@gmail.com>
2020-08-11 11:10:44 +0200

    Bump core services

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,49 +1,38 @@
 defmodule Dispatcher do
-  use Plug.Router
+  use Matcher
 
-  def start(_argv) do
-    port = 80
-    IO.puts "Starting Plug with Cowboy on port #{port}"
-    Plug.Adapters.Cowboy.http __MODULE__, [], port: port
-    :timer.sleep(:infinity)
-  end
+  define_accept_types [
+    any: [ "*/*" ]
+  ]
 
-  plug Plug.Logger
-  plug :match
-  plug :dispatch
+  @any %{ accept: %{ any: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule.
-  #
-  # docker-compose stop; docker-compose rm; docker-compose up
-  # after altering this file.
-  #
-  # match "/themes/*path" do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-
-  match "/complaint-forms/*path" do
-    Proxy.forward conn, path, "http://resource/complaint-forms/"
+  match "/complaint-forms/*path", @any do
+    forward conn, path, "http://resource/complaint-forms/"
   end
 
-  get "/files/:id/download" do
-    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
-  end
-  get "/files/*path" do
-    Proxy.forward conn, path, "http://resource/files/"
-  end
-  post "/file-service/files/*path" do
-    Proxy.forward conn, path, "http://file/files/"
-  end
-  delete "/files/*path" do
-    Proxy.forward conn, path, "http://file/files/"
-  end
-  match "/file-addresses/*path" do
-    Proxy.forward conn, path, "http://resource/file-addresses/"
+  get "/files/:id/download", @any do
+    forward conn, [], "http://file/files/" <> id <> "/download"
   end
 
-  match _ do
-    send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
+  get "/files/*path", @any do
+    forward conn, path, "http://resource/files/"
+  end
+
+  post "/file-service/files/*path", @any do
+    forward conn, path, "http://file/files/"
+  end
+
+  delete "/files/*path", @any do
+    forward conn, path, "http://file/files/"
+  end
+
+  match "/file-addresses/*path", @any do
+    forward conn, path, "http://resource/file-addresses/"
+  end
+
+  get "/*_", %{ last_call: true } do
+    send_resp( conn, 404, "{ \"error\": { \"code\": 404, \"message\": \"Route not found.  See config/dispatcher.ex\" } }" )
   end
 
 end
