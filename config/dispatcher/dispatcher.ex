4c01e1aef2d6c8e032af0d71a160d3445c60574b
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-09-08 13:06:29 +0200

    Security hotfix.
    There were too many combinations of endpoints and verbs publicly
    availible. They have been removed.
    
    Extra comments have been added, to emphasize what is protected
    by Nginx.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -7,28 +7,29 @@ defmodule Dispatcher do
 
   @any %{ accept: %{ any: true } }
 
-  match "/complaint-forms/*path", @any do
+  # Users only create complaint forms.
+  post "/complaint-forms/*path", @any do
     forward conn, path, "http://resource/complaint-forms/"
   end
 
+  # This will be protected by basic-auth, so file content will never be public.
   get "/files/:id/download", @any do
     forward conn, [], "http://file/files/" <> id <> "/download"
   end
 
+  # Returns meta data of the uploaded file.
   get "/files/*path", @any do
     forward conn, path, "http://resource/files/"
   end
 
+  # Endpoint required to upload file content.
   post "/file-service/files/*path", @any do
     forward conn, path, "http://file/files/"
   end
 
+  # User must be able to delete file.
   delete "/files/*path", @any do
     forward conn, path, "http://file/files/"
-  end
-
-  match "/file-addresses/*path", @any do
-    forward conn, path, "http://resource/file-addresses/"
   end
 
   get "/*_", %{ last_call: true } do
