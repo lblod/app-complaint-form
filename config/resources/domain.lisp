027ab4337b80274a34086e2e6acf663ca661a616
achraf <achrafatauil@hotmail.com>
2020-12-16 12:48:07 +0100

    fix newly added files import in domain.lisp file

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -5,23 +5,6 @@
 ;; docker-compose stop; docker-compose rm; docker-compose up
 ;; after altering this file.
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;; KLACHTENFORMULIEREN ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define-resource complaint-form ()
-  :class (s-prefix "ext:ComplaintForm")
-  :properties `((:name :string ,(s-prefix "foaf:name"))
-                (:contact-person-name :string ,(s-prefix "ext:personName"))
-                (:street :string ,(s-prefix "schema:streetAddress"))
-                (:house-number :number ,(s-prefix "schema:postOfficeBoxNumber"))
-                (:address-complement :string ,(s-prefix "ext:addressComplement"))
-                (:locality :string ,(s-prefix "schema:addressLocality"))
-                (:postal-code :number ,(s-prefix "schema:postalCode"))
-                (:telephone :string ,(s-prefix "schema:telephone"))
-                (:email :string ,(s-prefix "schema:email"))
-                (:content :string ,(s-prefix "ext:content"))
-                (:created :datetime ,(s-prefix "dct:created"))
-                (:is-converted-to-email :string ,(s-prefix "ext:isConvertedIntoEmail")))
-  :has-many `((file :via ,(s-prefix "nmo:hasAttachment")
-                    :as "attachments"))
-  :resource-base (s-url "http://data.lblod.info/complaint-forms/")
-  :on-path "complaint-forms")
+(read-domain-file "master-files-domain.lisp")
+(read-domain-file "master-email-domain.lisp")
+(read-domain-file "master-form-domain.lisp")
